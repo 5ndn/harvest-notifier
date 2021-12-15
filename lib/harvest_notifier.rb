b50a9190bc6c7d91c0b375406227a6d6eb68e181
Joel Caballero <joel@fiveanddone.com>
2021-12-14 16:34:36 -0800

    Fix: Bad call for today

--- lib/harvest_notifier.rb
+++ lib/harvest_notifier.rb
@@ -29,7 +29,7 @@ module HarvestNotifier
     #date_to = date_from + 4
 
     date_from = Date.today.beginning_of_week
-    date_to = Date.today.today
+    date_to = Date.today
 
     HarvestNotifier::Base.new.create_weekly_report(date_from, date_to)
   end
